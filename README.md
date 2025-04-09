343ce65b60cdc1f1b996b78eb8a1ac43f8d93f45
Marcus <marcus@proest.net>
2025-04-09 11:55:15 +0200

    update readme, project setup and npm scripts

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 * install [git](https://git-scm.com/downloads/win)
+* Install [nvm](https://github.com/coreybutler/nvm-windows)
+* `nvm install 20`
+* `nvm use 20`
 * open a cmd and go to some directory
 * run `git clone https://github.com/MarcProe/GlamBot.git`
 * `cd GlamBot`
-* run `setup.bat`
+* run `setup.bat` (will create some folders)
+* run `npm install`
 * create a file named `.env` and fill it
 * put intro and outro in folder `asset`
-* start with `node index.js`
+* start processor with `npm run prc`
+* start server with `npm run srv`
 * put some file into the configured folder (`E:\video`)
+* access the webinterface via `http://localhost:3000`
--- setup.bat
+++ setup.bat
@@ -1,35 +1,17 @@
 @echo off
 setlocal
 
-echo === [1/5] Downloading NVM for Windows installer ===
-set NVM_URL=https://github.com/coreybutler/nvm-windows/releases/latest/download/nvm-setup.exe
-set NVM_SETUP=%TEMP%\nvm-setup.exe
-
-powershell -Command "Invoke-WebRequest -Uri %NVM_URL% -OutFile '%NVM_SETUP%'"
-start /wait "" "%NVM_SETUP%"
-
 echo.
-echo === [2/5] Installing Node.js 20 via NVM ===
-nvm install 20
-nvm use 20
-
-echo.
-echo === [3/5] Creating required folders ===
+echo === Creating required folders ===
 mkdir input
 mkdir output
 mkdir thumbnails
 mkdir meta
-mkdir modules
-mkdir public
 mkdir views
 mkdir assets
 
 echo.
-echo === [4/5] Installing Node.js dependencies ===
-call npm install
-
-echo.
-echo === [5/5] Setup complete ===
+echo === Setup complete ===
 echo Project folder: %CD%
 pause
 endlocal
